bc0595e2d874c1761ebf011589b807bafa98de92
andrescaicedoeb-prog <andrescaicedo.eb@gmail.com>
2026-09-12 17:43:33 -0500

    feat: Actualiacion de App.jsx completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import ExplorarDestinos from "./pages/ExplorarDestinos";
+import MiListaDeViajes from "./pages/MiListaDeViajes";
 import "./styles.css";
 
 function App() {
+  const [actualizar, setActualizar] = useState(0);
+
   return (
     <main>
       <h1>Explora Destinos</h1>
-      <ExplorarDestinos />
+
+      <ExplorarDestinos
+        onDestinoAgregado={() =>
+          setActualizar((valor) => valor + 1)
+        }
+      />
+
+      <hr />
+
+      <MiListaDeViajes actualizar={actualizar} />
     </main>
   );
 }
 
 export default App;
-
-// integracion de App.jsx de destinos-react
